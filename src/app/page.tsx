e046bf20b23cdf092be1c1f604d6938407303628
Nellie Allsop <nellie.allsop@gmail.com>
2023-12-10 16:31:41 +0000

    add image and change meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>Nellie Allsop&apos;s portfolio site</div>
+			<div>Nellie Allsop | portfolio</div>
+			<Image className="rounded border-8 border-orange-400 m-8" src='/presentation.jpg' alt='Nellie presenting during her graduation from a full stack bootcamp' height={400} width={387}></Image>
 		</main>
 	);
 }
