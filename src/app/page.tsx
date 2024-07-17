6eee20420a144d48e28df88fe71a3004e70ee02f
Nellie Allsop <nellie.allsop@gmail.com>
2024-07-17 18:43:06 +0100

    add copyright notice to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import WhatYouCanDo from "@/components/WhatYouCanDo";
 import Contact from "@/components/Contact";
 import TheEvidence from "@/components/TheEvidence";
 import AboutNellie from "@/components/AboutNellie";
-import Header from "@/components/Header"
+import Header from "@/components/Header";
 
 export default function Home() {
 	return (
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function Footer () {
-  return (
-    <footer className="flex p-4 justify-around">
-
-  <p>Find me on LinkedIn 
-  <Link href="https://www.linkedin.com/in/nellie-allsop/" className="font-extrabold text-pink"> here</Link>
-  </p>
-    </footer>
-  )
+export default function Footer() {
+	return (
+		<footer className="flex flex-col p-4 justify-center items-center">
+			<p className="p-2">
+				Find me on LinkedIn
+				<Link
+					href="https://www.linkedin.com/in/nellie-allsop/"
+					className="font-extrabold text-pink"
+				>
+					{" "}
+					here
+				</Link>
+			</p>
+			<p>&copy; 2024 Nellie Allsop</p>
+		</footer>
+	);
 }
